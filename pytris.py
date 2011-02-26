a598d3ebd5cc422d56ba8591ff6e254a8436bd2a
Vayn <vt@elnode.com>
2011-02-26 23:27:50 +0800

    0.1.0

---
+++ pytris.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# vim:fileencoding=utf-8
+# @Name: pytris.py
+# @Date: 2011年02月26日 星期六 15时56分56秒
+
+import os
+import pygame
+import states
+from pygame.locals import *
+
+__author__ = "Vayn a.k.a. VT"
+__copyright__ = "Copyright (c) 2011, Vincent Tsai"
+
+__license__ = "GPL"
+__version__ = "0.1.0"
+__email__ = "vayn@vayn.de"
+__status__ = "Development"
+
+__metaclass__ = type
+
+class Game:
+    def __init__(self):
+        self.dir = os.path.split(os.path.abspath(__file__))[0]
+        os.chdir(self.dir)
+        self.state = None
+        self.nextState = states.StartUp()
+
+    def run(self):
+        os.environ["SDL_VIDEO_CENTERED"] = "1"
+        pygame.init()
+
+        clock = pygame.time.Clock()
+        screen = pygame.display.set_mode((states.WINDOWWIDTH, states.WINDOWHEIGHT), NOFRAME)
+        pygame.display.set_caption('Pytris ' + __version__)
+        pygame.mouse.set_visible(False)
+
+        icon = pygame.image.load(states.loadData('icon.png', 'image')).convert()
+        icon = pygame.transform.scale(icon, (32, 32))
+        pygame.display.set_icon(icon)
+
+        while True:
+            clock.tick(states.FPS)
+
+            if self.state != self.nextState:
+                self.state = self.nextState
+                self.state.firstDisplay(screen)
+            for event in pygame.event.get():
+                self.state.handle(event)
+            self.state.update(self)
+            self.state.display(screen)
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
+
---
+++ tetrominoes.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python
+# vim:fileencoding=utf-8
+# @Author: Vayn a.k.a. VT <vayn@vayn.de>
+# @Name: tetrominoes.py
+# @Date: 2011年02月26日 星期六 15时57分20秒
+
+# Constant variables
+FPS          = 25
+WINDOWWIDTH  = 640
+WINDOWHEIGHT = 480
+BLOCKSIZE    = 20
+BOARDWIDTH   = 10
+BOARDHEIGHT  = 20
+BLANK        = -1
+
+
+# Key repeat frequency
+MOVESIDEWAYSFREQ = 0.15
+MOVEDOWNFREQ     = 0.15
+
+
+# Board position
+XMARGIN = int((WINDOWWIDTH - BOARDWIDTH * BLOCKSIZE) / 2)
+BOARDTOP = WINDOWHEIGHT - (BOARDHEIGHT * BLOCKSIZE) - 5
+
+
+# Palette
+WHITE    = (255, 255, 255)
+BLACK    = (  0,   0,   0)
+RED      = (239,  52,  52)
+GREEN    = (  0, 191,   0)
+BLUE     = (  0,  85, 255)
+YELLOW   = (255, 246,   0)
+GRAY     = (191, 189, 189)
+DARKGRAY = (219, 219, 219)
+
+BORDERCOLOR = BLACK
+BGCOLOR = GRAY
+COLORS = (BLUE, GREEN, RED, YELLOW)
+
+# Shape (5 * 5)
+S_SHAPE = [['.....',
+            '.....',
+            '..OO.',
+            '.OO..',
+            '.....'],
+           ['.....',
+            '..O..',
+            '..OO.',
+            '...O.',
+            '.....']]
+
+Z_SHAPE =[['.....',
+           '.....',
+           '.OO..',
+           '..OO.',
+           '.....'],
+          ['.....',
+           '..O..',
+           '.OO..',
+           '.O...',
+           '.....']]
+
+I_SHAPE =[['..O..',
+           '..O..',
+           '..O..',
+           '..O..',
+           '.....'],
+          ['.....',
+           '.....',
+           'OOOO.',
+           '.....',
+           '.....']]
+
+O_SHAPE =[['.....',
+           '.....',
+           '.OO..',
+           '.OO..',
+           '.....']]
+
+J_SHAPE = [['.....',
+            '.O...',
+            '.OOO.',
+            '.....',
+            '.....'],
+           ['.....',
+            '..OO.',
+            '..O..',
+            '..O..',
+            '.....'],
+           ['.....',
+            '.....',
+            '.OOO.',
+            '...O.',
+            '.....'],
+           ['.....',
+            '..O..',
+            '..O..',
+            '.OO..',
+            '.....']]
+
+L_SHAPE = [['.....',
+            '...O.',
+            '.OOO.',
+            '.....',
+            '.....'],
+           ['.....',
+            '..O..',
+            '..O..',
+            '..OO.',
+            '.....'],
+           ['.....',
+            '.....',
+            '.OOO.',
+            '.O...',
+            '.....'],
+           ['.....',
+            '.OO..',
+            '..O..',
+            '..O..',
+            '.....']]
+
+T_SHAPE = [['.....',
+            '..O..',
+            '.OOO.',
+            '.....',
+            '.....'],
+           ['.....',
+            '..O..',
+            '..OO.',
+            '..O..',
+            '.....'],
+           ['.....',
+            '.....',
+            '.OOO.',
+            '..O..',
+            '.....'],
+           ['.....',
+            '..O..',
+            '.OO..',
+            '..O..',
+            '.....']]
+
+SHAPES = {'S': S_SHAPE,
+          'Z': Z_SHAPE,
+          'J': J_SHAPE,
+          'L': L_SHAPE,
+          'I': I_SHAPE,
+          'O': O_SHAPE,
+          'T': T_SHAPE}
+
+for p in SHAPES:                    # loop through each shape
+    for i in range(len(SHAPES[p])): # loop through each rotation of the shape
+        shapeData = []
+        for x in range(5):          # loop through each column of the rotation
+            column = []
+            for y in range(5):      # loop through each character of the column
+                if SHAPES[p][i][x][y] == '.':
+                    column.append(BLANK)
+                else:
+                    column.append(1)
+            shapeData.append(column)
+        SHAPES[p][i] = shapeData
+
